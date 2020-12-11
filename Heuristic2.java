cc42ab5bef16ece747dd3631f82f3b5996d20d75
Mhd.Anggie Rinovka <45094075+MhdAnggieRinovka@users.noreply.github.com>
2020-12-11 22:32:22 +0700

    Create Heuristic2.java

---
+++ Heuristic2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS FX504
+ */
+
+public class Heuristic2 extends Heuristic{
+	
+	@Override
+	public int getValue(State state) { //mendapatkan value dari state
+		return 0; //mengembalikan nilai 0 untuk menyatakan status awalnya
+	}
+
+}
