c9b56464f364aa20bcd05839a2718f42490262a3
Mhd.Anggie Rinovka <45094075+MhdAnggieRinovka@users.noreply.github.com>
2020-12-11 22:30:01 +0700

    Create Heuristic.java

---
+++ Heuristic.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS FX504
+ */
+
+public abstract class Heuristic {
+	
+	public Heuristic() {}
+	
+	public abstract int getValue(State state); //abstract class yang dimana fungsi ini bertujuan untuk mengambil value;
+		
+}
